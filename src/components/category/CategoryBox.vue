3ec4ebdc6524b8e0716a70b885c961e625eb10ad
faisalibal33 <faisaliqbal030397@gmail.com>
2022-09-10 23:32:38 +0700

    add category page

---
+++ src/components/category/CategoryBox.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="categoryCard">
+    <div class="cardImg">
+      <img :src="category.imageUrl" alt="" />
+    </div>
+    <div class="categoryName">
+      <h1>{{ category.categoryName }}</h1>
+    </div>
+    <div class="categoryDesc">
+      <p>{{ category.description }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CategoryBox",
+  props: ["category"],
+  methods: {},
+};
+</script>
+
+<style>
+.categoryCard {
+  border: 1px solid blue;
+  height: 300px;
+  width: 300px;
+}
+
+.cardImg {
+  width: 100%;
+  height: 50%;
+}
+
+.cardImg img {
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+}
+</style>
--- src/views/Category/Category.vue
+++ src/views/Category/Category.vue
@@ -1,9 +1,53 @@
 <template>
-  <div>Categories ISI nya</div>
+  <div class="c-container">
+    <h2>Category</h2>
+    <div class="categoryView">
+      <div v-for="category of categories" :key="category.id">
+        <CategoryBox :category="category" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+import CategoryBox from "../../components/category/CategoryBox.vue";
+export default {
+  name: "Category",
+  components: { CategoryBox },
+  data() {
+    return {
+      categories: [],
+    };
+  },
+  methods: {
+    async getCategories() {
+      await axios
+        .get("https://limitless-lake-55070.herokuapp.com/category/")
+        .then((res) => (this.categories = res.data))
+        .catch((error) => console.log(error));
+    },
+  },
+  mounted() {
+    this.getCategories();
+  },
+};
 </script>
 
-<style></style>
+<!-- style -->
+<style>
+.c-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: auto;
+  max-width: 1300px;
+}
+
+.categoryView {
+  display: grid;
+  grid-template-columns: 33% 33% 33%;
+  gap: 20px;
+}
+</style>
